3af5f756b4ed28ac36a5fc8b1a546221f5268e95
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-08-10 02:35:44 -0700

    obj storage

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 # AbdulTechX
 import uuid
 from datetime import datetime
+from models import storage
 """define a class BaseModel"""
 
 
@@ -39,10 +40,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """updates the public instance attribute with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
