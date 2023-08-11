be42cece1154846c4375d4217fe7e19cecfb517e
Deejaht <ravenroth486@gmail.com>
2023-08-11 15:50:13 +0000

    Update status

---
+++ models/state.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+# state.py
+# AbdulTechX
+""""Define a class User that inhert from BaseModel"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a User.
+
+    Attributes:
+           name(str): the name of the state
+    """
+    name = ""
+
