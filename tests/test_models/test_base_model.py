83d8fe23d845b02f71e2e1b87e068beb8407dd70
Deejaht <ravenroth486@gmail.com>
2023-08-12 19:28:59 +0000

    testing

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""Unittest module for the BaseModel Class."""
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+import json
+import os
+import re
+import time
+import unittest
+import uuid
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test Cases for the BaseModel class."""
+
+
+if __name__ == '__main__':
+    unittest.main()
+
